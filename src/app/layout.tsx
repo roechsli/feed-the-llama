f41c2bf34c9c899be19d2efa29e6096c6853c95e
Roman Oechslin <romanoech@gmail.com>
2024-12-01 11:07:13 +0100

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { FeedTheLlama } from "@/components/ui/logo-text";
 
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <div className="md:flex-col flex-col-reverse">
           <FeedTheLlama />
+          <Analytics />
           {children}
         </div>
       </body>
